5b51ea416dfa1137172bbe7e675a6f7cecb4a5c8
vgorthi <gorthivani2020@gmail.com>
2023-08-10 15:19:39 +0530

    variables map

--- 03-var/sample.tf
+++ 03-var/sample.tf
@@ -1,3 +1,5 @@
+# plain variable
+
 variable "fruit_name" {
   default = "apple"
 }
@@ -5,3 +7,32 @@ variable "fruit_name" {
 output "fruit_name" {
   value = var.fruit_name
 }
+# list variables
+variable "fruits" {
+  default = [
+    apple,
+    banana]
+  // default = [apple,banana] // single line synatx
+}
+
+# map variable plain
+variable "fruit_stock" {
+  default = {
+    apple=100,
+    banana=200
+  }
+}
+#map variables map of maps
+variable "fruit_stock_with_price" {
+  default = {
+    apple = {
+      stock = 100,
+      price = 3
+    }
+      banana ={
+        stock= 400,
+        price= 1
+      }
+  }
+  }
+
