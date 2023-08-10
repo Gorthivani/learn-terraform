e418bb578b2b3144965f30b68de3368f5b1b8b11
vgorthi <gorthivani2020@gmail.com>
2023-08-10 16:14:49 +0530

    variables map

--- 03-var/sample.tf
+++ 03-var/sample.tf
@@ -52,3 +52,21 @@ output "fruit_stock_with_price_of_apple" {
   value = var.fruit_stock_with_price["apple"].stock
 }
 
+#  variable datatypes
+variable "fruit_details" {
+  default = {
+    apple= {
+      stock= 100 // number
+      type= "washington" // string
+      for_sale=true // boolean
+    }
+
+  }
+}
+# variable in a combination  any other strings then it needs to be dollar parenthis ${}
+output "fruit_name_1" {
+  value = " fruit name =${var.fruit_name}"
+}
+output "fruit_details_apple" {
+  value = "Apple stock=  ${var.fruit_details["apple"].stock} , Apple type=${var.fruit_details[apple].type}, apple sale status =${var.fruit_details["apple"].for_sale}"
+}
