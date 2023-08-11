d2d9d2c98be330a770ab24a749eec955d8556e93
vgorthi <gorthivani2020@gmail.com>
2023-08-11 15:51:22 +0530

    module

--- 07-module/local-module/data.tf
+++ 07-module/local-module/data.tf
@@ -1,5 +1,18 @@
-data "aws_ami_ids" "ami"{
-  name_regex = "centos-*"
+data "aws_ami" "ami"{
+  most_recent = true
+  name_regex = "Centos-8-DevOps-Practice"
   owners = ["194160465942"]
 }
+output "ami" {
+  value = data.aws_ami.ami
+}
 
+resource "aws_instance" "instance" {
+
+ami           =  data.aws_ami.ami.id
+instance_type = var.instance_type
+vpc_security_group_ids = [ "sg-0ee67709785a4f0d8" ]
+
+
+}
+variable "instance_type" {}
