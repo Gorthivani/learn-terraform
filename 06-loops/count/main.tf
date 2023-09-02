18741acf361b4363a0cd09723d25194458a199e9
vgorthi <gorthivani2020@gmail.com>
2023-09-02 23:55:32 +0530

    nat gateway

--- 06-loops/count/main.tf
+++ 06-loops/count/main.tf
@@ -1,17 +1,17 @@
 variable "components" {
   default = ["frontend", "mongodb"]
 }
- //resource "aws_instance" "instance" {
-   //count = length(var.components)
-   //ami           = "ami-03265a0778a880afb"
-   //instance_type = "t3.micro"
-   //vpc_security_group_ids = [ "sg-0ee67709785a4f0d8" ]
+ resource "aws_instance" "instance" {
+   count = length(var.components)
+   ami           = "ami-03265a0778a880afb"
+   instance_type = "t3.micro"
+   vpc_security_group_ids = [ "sg-0ee67709785a4f0d8" ]
 
-   //tags = {
-     //Name = var.components[count.index]
-     //Name = element(var.components,count.index )
-   //}
- //}
+   tags = {
+#     Name = var.components[count.index]
+     Name = element(var.components,count.index )
+   }
+ }
 resource "aws_security_group" "allow_tls" {
   count = length(var.components)
   name        = element(var.components,count.index )
